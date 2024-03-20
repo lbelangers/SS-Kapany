0cc5b8c69f06e9e91107a1ad475f11fd053e8721
Louis Bélanger Sansoucy <belanger.sansoucy.louis@gmail.com>
2024-03-20 00:10:05 -0400

    Beginning simulation, not working

--- src/exploration.py
+++ src/exploration.py
@@ -1,15 +1,20 @@
 import numpy as np
 
-
 def test_func(x):
     """This function will try to calculate:
 
     .. math::
-        \sum_{i=1}^{\\infty} x_{i}
+        \sum_{i=1}^{\infty} x_{i}
 
     good luck!
     """
     pass
 
+E0 = 1
 
-test_func()
+def rect(t: np.ndarray):
+    return (t > 0) & (t < 1)
+
+a = np.linspace(0, 2, 100)
+
+print(rect(a))
